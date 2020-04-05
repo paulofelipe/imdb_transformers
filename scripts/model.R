183421cff6522636cc182d23d74ffa3dce35d511
paulofelipe <paulofel2011@gmail.com>
2020-04-05 18:23:14 -0300

    increase maximum text length

--- scripts/model.R
+++ scripts/model.R
@@ -4,6 +4,7 @@ library(tensorflow)
 transformers <- import("transformers")
 
 transformer_layer <- transformers$TFBertModel$from_pretrained("bert-base-cased")
+#transformer_layer$trainable <- FALSE
 
 build_model <- function(
   transformer = transformer_layer,
@@ -17,11 +18,13 @@ build_model <- function(
     name = "input_words_ids"
   )
   pooled_output <- transformer(input_word_ids)[[2]]
-  x <- tf$keras$layers$Dropout(0.25)(pooled_output)
-  out <- tf$keras$layers$Dense(units = 1L, activation = "sigmoid")(x)
+  #x <- tf$keras$layers$Dropout(0.05)(pooled_output)
+  #x <- tf$keras$layers$Dense(units = 1028L, activation = "relu")(pooled_output)
+  #x <- tf$keras$layers$Dropout(0.05)(x)
+  out <- tf$keras$layers$Dense(units = 1L, activation = "sigmoid")(pooled_output)
 
   model <- tf$keras$Model(inputs = input_word_ids, outputs = out)
-  model$compile(tf$keras$optimizers$Adam(lr = 3e-5), loss = loss, metrics = list("accuracy"))
+  model$compile(tf$keras$optimizers$Adam(lr = 1e-5), loss = loss, metrics = list("accuracy"))
   model
 }
 
--- scripts/train_model.R
+++ scripts/train_model.R
@@ -6,26 +6,27 @@ library(tfdatasets)
 source("scripts/prepare_input.R")
 source("scripts/model.R")
 
+max_len <- 256L
+batch_size <- 32L
+
 # Load the data -------------------------------------------------------
 imdb_data <- read_csv("data/imdb_data.csv")
 
 train <- imdb_data %>%
-  filter(dataset == "train") %>% 
-  sample_n(5000)
+  filter(dataset == "train")
 
 test <- imdb_data %>%
-  filter(dataset == "test") %>%
-  sample_n(1000)
+  filter(dataset == "test")
 
 # Prepare inputs and targets ---------------------------------------------
 x_train <- fast_encode(
   texts = train$review,
-  max_len = 128L
+  max_len = max_len
 )
 
 x_test <- fast_encode(
   texts = test$review,
-  max_len = 128L
+  max_len = max_len
 )
 
 y_train <- train %>%
@@ -41,20 +42,21 @@ y_test <- test %>%
 # TF datasets --------------------------------------------------------------
 
 train_ds <- tensor_slices_dataset(tensors = list(x_train, y_train)) %>% 
-  dataset_repeat() %>% 
-  dataset_batch(32)
+  #dataset_repeat() %>% 
+  dataset_shuffle(512) %>% 
+  dataset_batch(batch_size)
 
 test_ds <- tensor_slices_dataset(tensors = list(x_test, y_test)) %>% 
-  dataset_batch(32)
+  dataset_batch(batch_size)
 
 # Model --------------------------------------------------------------------
 
-model <- build_model(max_len = 128L)
+model <- build_model(max_len = max_len)
 model$summary()
 
 train_history <- model$fit(
   train_ds,
-  steps_per_epoch = 156L,
+  #steps_per_epoch = 10L,
   validation_data = test_ds,
-  epochs = 3L
+  epochs = 30L
 )
